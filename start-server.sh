46ac1723adddcd98be9d4f6b5ef5f126ee7d05c5
Mona5789 <mona.parvam@gmail.com>
2025-05-19 18:15:50 +0530

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-set -e
+set -ex  # <-- enables debug + exit on error
 
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 
+echo "Creating superuser if env vars present"
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    python manage.py createsuperuser --no-input
+    python manage.py createsuperuser --no-input || true
 fi
 
+echo "Collecting static files"
 python manage.py collectstatic --no-input
 
+echo "Starting Gunicorn"
 exec gunicorn grasptek.wsgi:application --bind 0.0.0.0:8000 --workers 5
