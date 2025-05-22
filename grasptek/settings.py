df2dfcd2efa51269cf57c28239494ec19b0aa4d6
Mona <mona9121992@gmail.com>
2025-05-22 08:41:05 +0530

    impproved static setting

--- grasptek/settings.py
+++ grasptek/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
 }
 LOGIN_REDIRECT_URL = '/profile/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 AUTH_PASSWORD_VALIDATORS = [
